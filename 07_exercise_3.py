03305dd18aa632720f6bff86b960d6a767f9db3e
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-08 05:24:41 +0530

    complete exercise 3

---
+++ 07_exercise_3.py
@@ -0,0 +1,18 @@
+# madlibs game
+# word game where you create a story by filling in the blanks with random words
+
+print("")
+print("A _____ found a shiny key.")
+print("It opened a _____ box.")
+print("Inside was a map to a hidden _____.")
+print("")
+
+
+animal = input("Enter a animal name: ")
+adjective1 = input("Enter first adjective: ")
+adjective2 = input("Enter second adjective: ")
+
+print("")
+print(f"A {animal} found a shiny key.")
+print(f"It opened a {adjective1} box.")
+print(f"Inside was a map to a hidden {adjective2}.")
