a70e8e88c37e0ce96eeaf494dd7027bcde208acc
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-08 05:05:05 +0530

    learn type casting

---
+++ 03_type_casting.py
@@ -0,0 +1,20 @@
+name = "IndieDev"
+age = 19
+gpa = 9.5
+is_developer = True
+blank = ""
+
+# type check
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_developer))
+
+# type casting
+print(str(gpa))
+print(int(gpa))
+print(float(age))
+print(int(is_developer))
+print(bool(name))
+print(bool(blank))
+print(str(age) + "1")
