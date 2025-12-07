623af577c24e2e1db81756e723d7876911560e96
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-08 05:10:58 +0530

    complete exercise 1

---
+++ 05_exercise_1.py
@@ -0,0 +1,7 @@
+# rectangle area calculator
+
+length = float(input("Enter length of the rectangle (cm): "))
+width = float(input("Enter width of the rectangle (cm): "))
+
+area = length * width
+print(f"Area of rectangle with your given dimensions is {area}cm²")
