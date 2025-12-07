ec6fc9e9bf8ba74a5ad214070b83b74a1ddcccdb
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-08 04:59:52 +0530

    learn variables

---
+++ 02_variables.py
@@ -0,0 +1,21 @@
+# variables with string value
+first_name = "Indie"
+last_name = "Dev"
+print(first_name + last_name)
+
+# f-string
+print(f"Hello {first_name+last_name}")
+
+# variable with integer value
+age = 19
+print(f"Hey! you are {age} years old.")
+
+# variable with float value
+attendance = 90.5  # float
+print(f"Your attendance is {attendance}%")
+
+# variables with boolean value
+is_developer = True
+is_fresher = False
+print(is_developer)
+print(is_fresher)
