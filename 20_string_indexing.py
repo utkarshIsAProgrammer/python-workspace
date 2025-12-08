e026c6f7028485450454dde411c1c7529fe32d29
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-08 09:14:53 +0530

    learn string indexing

---
+++ 20_string_indexing.py
@@ -0,0 +1,14 @@
+# string indexing
+
+name = "IndieDev"
+
+if name[0].islower():
+    name = name.capitalize()
+
+first_name = name[:3].upper()
+last_name = name[4:].lower()
+last_character = name[-1]
+
+print(first_name, last_name)
+
+print(name)
