3e35f487e0c226725f51fd2523656b03277934f3
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-08 08:35:54 +0530

    learn logical operators

---
+++ 17_logical_operators.py
@@ -0,0 +1,9 @@
+# logical operators
+
+print(5 > 6 and 6 > 5)  # False
+print(5 < 6 and 6 > 5)  # True
+
+print(5 > 6 or 6 > 5)  # True
+print(5 < 6 or 6 > 5)  # True
+
+print(not (5 < 6 and 6 > 5))  # False
