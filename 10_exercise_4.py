71b3fe32f3ccdd7c284a2a0917904ca1fb087b23
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-08 05:51:14 +0530

    complete exercise 4

---
+++ 10_exercise_4.py
@@ -0,0 +1,10 @@
+# circumference of circle
+
+import math
+
+pi = math.pi
+
+radius = float(input("Enter the radius of the circle (cm): "))
+circumference = 2 * pi * radius
+
+print(f"Circumference: {round(circumference,2)}cm")
