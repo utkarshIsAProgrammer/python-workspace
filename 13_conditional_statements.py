3870a79077185c49859899db51db5f683028a7f8
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-08 06:03:31 +0530

    learn conditional statements

---
+++ 13_conditional_statements.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+if age > 120:
+    print("You are too old to watch this content!")
+elif age >= 18:
+    print("You can watch this movie.")
+elif age < 0:
+    print("You don't exist in this world!")
+else:
+    print("You cannot watch this movie")
