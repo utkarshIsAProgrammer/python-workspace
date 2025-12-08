de3be518e2fb4fd8b7a3ac180f55d79478fe4dc3
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-08 17:26:56 +0530

    learn __name__ == __main__

---
+++ 42_main.py
@@ -0,0 +1,6 @@
+import __math_utils
+
+
+print("Importing and using math_utils:")
+print("Addition:", __math_utils.add(20, 10))
+print("Subtraction:", __math_utils.subtract(20, 10))
---
+++ __math_utils.py
@@ -0,0 +1,17 @@
+# math_utils.py
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def main():
+    print("Running as a script:")
+    print("Addition:", add(10, 5))
+    print("Subtraction:", subtract(10, 5))
+
+
+if __name__ == "__main__":
+    main()
