af72c5a3bbe7b29b7bf3e494f9c0550127b60e48
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-08 09:51:14 +0530

    learn tuple

---
+++ 27_tuple.py
@@ -0,0 +1,12 @@
+# tuple
+
+student = ("Bro", 21, "male")
+
+print(student.count(21))
+print(student.index("male"))
+
+for x in student:
+    print(x)
+
+if "Bro" in student:
+    print("Bro is here")
