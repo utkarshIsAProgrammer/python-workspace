0d252e6d88bf245cc93f33e8e9ba435835211b34
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-08 05:48:14 +0530

    learn some math functions

---
+++ 09_math_functions.py
@@ -0,0 +1,16 @@
+import math
+
+x = 5
+y = 4.5
+pi = math.pi  # value of pi
+
+print(round(pi))
+print(round(pi, 2))
+
+print(pow(x, 2))
+print(max(2, 8, 6))
+print(min(2, 8, 6))
+
+print(math.sqrt(x))
+print(math.ceil(y))  # rounds ups to the nearest integer
+print(math.floor(y))  # rounds down to the nearest integer
