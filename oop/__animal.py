2cc45f387b7c06ce4da25d0b7774fc0c17c595c3
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-10 07:44:29 +0530

    lear inheritance

---
+++ oop/__animal.py
@@ -0,0 +1,44 @@
+# base class
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.is_alive = True
+
+    def eat(self):
+        return f"{self.name} is eating."
+
+    def sleep(self):
+        return f"{self.name} is sleeping."
+
+
+# derived class
+class Dog(Animal):
+    def speak(self):
+        return "Woof!"
+
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow!"
+
+
+class Mouse(Animal):
+    def speak(self):
+        return "Squeak!"
+
+
+dog = Dog("Caesar")
+cat = Cat("Tom")
+mouse = Mouse("Mickey")
+
+print(dog.name)
+print(dog.sleep())
+print(dog.speak())
+
+print(cat.name)
+print(cat.sleep())
+print(cat.speak())
+
+print(mouse.name)
+print(mouse.eat())
+print(mouse.speak())
